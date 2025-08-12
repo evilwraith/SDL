1fd0b8aab52babb24747fc2515ad237220301f74
Wes Smith <evilwraith@gmail.com>
2025-08-12 19:19:44 -0400

    adding the symbol I hope

--- include/SDL3/SDL_kmsdrm_helpers.h
+++ include/SDL3/SDL_kmsdrm_helpers.h
@@ -1,24 +1,17 @@
-/* Simple, public helper to fetch GBM handles for a KMSDRM-backed SDL_Window. */
-#ifndef SDL_KMSDRM_HELPERS_H_
-#define SDL_KMSDRM_HELPERS_H_
-
 #include <SDL3/SDL_stdinc.h>
 #include <SDL3/SDL_video.h>
+struct gbm_device;
+struct gbm_surface;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Forward declarations keep this header independent of <gbm.h>. */
-struct gbm_device;
-struct gbm_surface;
-
-/* Returns 0 on success, non-zero on failure. On success both out pointers are non-NULL. */
-SDL_DECLSPEC int SDLCALL SDL_KMSDRM_GetGBMHandles(SDL_Window *window,
-                                                  struct gbm_device **out_dev,
-                                                  struct gbm_surface **out_surf);
+SDL_DECLSPEC int SDLCALL
+SDL_KMSDRM_GetGBMHandles(SDL_Window *window,
+                         struct gbm_device **out_dev,
+                         struct gbm_surface **out_surf);
 
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
-#endif /* SDL_KMSDRM_HELPERS_H_ */
